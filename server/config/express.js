e343fb667a1227cb3c114b0152142788a8e9890c
sani <khursani8@gmail.com>
2017-05-12 07:20:53 +0800

    Built efwaipee from commit c8c135d on branch develop

--- server/config/express.js
+++ server/config/express.js
@@ -50,20 +50,20 @@ exports.default = function (app) {
    * Lusca - express server security
    * https://github.com/krakenjs/lusca
    */
-  if (env !== 'test' && !process.env.SAUCE_USERNAME) {
-    app.use((0, _lusca2.default)({
-      csrf: {
-        angular: true
-      },
-      xframe: 'SAMEORIGIN',
-      hsts: {
-        maxAge: 31536000, //1 year, in seconds
-        includeSubDomains: true,
-        preload: true
-      },
-      xssProtection: true
-    }));
-  }
+  // if(env !== 'test' && !process.env.SAUCE_USERNAME) {
+  //   app.use(lusca({
+  //     csrf: {
+  //       angular: true
+  //     },
+  //     xframe: 'SAMEORIGIN',
+  //     hsts: {
+  //       maxAge: 31536000, //1 year, in seconds
+  //       includeSubDomains: true,
+  //       preload: true
+  //     },
+  //     xssProtection: true
+  //   }));
+  // }
 
   if (env === 'development') {
     var webpackDevMiddleware = require('webpack-dev-middleware');

1ad370e332bb522d4a3723d9596413d3ffcafc55
sani <khursani8@gmail.com>
2017-06-09 02:45:44 +0800

    Built efwaipee from commit 1a75d76 on branch develop

--- server/api/thesis/index.js
+++ server/api/thesis/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
+var _auth = require('../../auth/auth.service');
+
+var auth = _interopRequireWildcard(_auth);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 var express = require('express');
 var controller = require('./thesis.controller');
 
+
 var router = express.Router();
 
-router.get('/', controller.index);
+router.get('/', auth.hasRole('admin'), controller.index);
 router.get('/name/:name', controller.showThesis);
 router.get('/:id', controller.show);
 router.get('/studentId/:id', controller.showStudent);
